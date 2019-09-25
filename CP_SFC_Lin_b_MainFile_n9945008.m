850ddef35b812e5f101f4bfe01db99ca46d657d0
csmithcripps <codysmithcripps@outlook.com>
2019-09-25 20:24:30 +1000

    Started Linearisation B

--- CP_SFC_Lin_b_MainFile_n9945008.m
+++ CP_SFC_Lin_b_MainFile_n9945008.m
@@ -8,35 +8,35 @@ l  = 0.2;
 g  = 9.81;
 
 %% Compute Linearisation Matrices
-Aa = [ 0 ,       0            , 1 , 0 ;
-       0 ,       0            , 0 , 1 ;
-       0 ,  (-(g*m)/Mc)       , 0 , 0 ;
-       0 , ((g*(Mc+m))/(l*Mc)), 0 , 0 ];
+Ab = [ 0 ,        0           , 1 , 0 ;
+       0 ,        0           , 0 , 1 ;
+       0 ,   (-(g*m)/Mc)      , 0 , 0 ;
+       0 ,  (-g*(Mc+m))/(l*Mc), 0 , 0 ];
    
-Ba = [0;0;1/Mc;-1/(l*Mc)];
+Bb = [0;0;1/Mc;1/(l*Mc)];
 
-Ca = eye(4);
+Cb = eye(4);
 
-Da = zeros(4,1);
+Db = zeros(4,1);
 
 
 %% Check Controlability
-controllableStates = rank(ctrb(Aa,Ba));
+controllableStates = rank(ctrb(Ab,Bb));
 
-if controllableStates == length(Aa)
+if controllableStates == length(Ab)
     disp("This System is Controllable")
 else
     disp("This System is not Controllable")
 end
 
 %% Place Poles
-Ka = place(Aa,Ba, [-3,-4,-5,-6]);
-K_SF = Ka;
-
+Kb = place(Ab,Bb, [-3,-4,-5,-6]);
+K_SF = Kb;
+setPoint = pi;
 
 %% Simulate Models
-x0 = [0.2 20*pi/180 0 0]';
-sim('CP_SFC_Lin_a_n9945008');
+x0 = [0.4 200*pi/180 0 0]';
+sim('CP_SFC_Lin_b_n9945008');
 sim('CP_SFC_NLin_n9945008');
 
 %% Plot Results
@@ -46,36 +46,36 @@ hold on
 subplot(5,1,1)
 hold on
 plot(t,x1)
-plot(ta,x1a)
-legend('x1','x1a')
+plot(tb,x1b)
+legend('x1','x1b')
 hold off
 
 subplot(5,1,2)
 hold on
 plot(t,x2)
-plot(ta,x2a)
-legend('x2','x2a')
+plot(tb,x2b)
+legend('x2','x2b')
 hold off
 
 subplot(5,1,3)
 hold on
 plot(t,x3)
-plot(ta,x3a)
-legend('x3','x3a')
+plot(tb,x3b)
+legend('x3','x3b')
 hold off
 
 subplot(5,1,4)
 hold on
 plot(t,x4)
-plot(ta,x4a)
-legend('x4','x4a')
+plot(tb,x4b)
+legend('x4','x4b')
 hold off
 
 subplot(5,1,5)
 hold on
 plot(t,F)
-plot(ta,Fa)
-legend('F','Fa')
+plot(tb,Fb)
+legend('F','Fb')
 hold off
 
 
